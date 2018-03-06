2a6a0ab62832b6e86ab91ab5b8f6eac81cac9451
FranciscoPonceGomez <pakopongo@gmail.com>
2018-03-05 21:49:14 -0800

    Added player to scene

--- Wolfenstein3D/Wolfenstein3D.js
+++ Wolfenstein3D/Wolfenstein3D.js
@@ -28,111 +28,114 @@ var map = [
 	[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
-var player = {
-	x : 16,			// current x, y position
-	y : 10,
-	dir : 0,		// the direction that the player is turning, either -1 for left or 1 for right.
-	rot : 0,		// the current angle of rotation
-	speed : 0,		// is the playing moving forward (speed = 1) or backwards (speed = -1).
-	moveSpeed : 0.18,	// how far (in map units) does the player move each step/update
-	rotSpeed : 6		// how much does the player rotate each step/update (in degrees)
+var mapWidth = 0;		// Number of map blocks in x-direction
+var mapHeight = 0;		// Number of map blocks in y-direction
+var miniMapScale = 8;	// How many pixels to draw a map block
+
+var Player = {
+	x : 10,
+	y : 15,
+	direction : 0,
+	rotation : 0,
+	speed : 0,
+	moveSpeed : 0.18,
+	rotaSpeed : 6 * Math.PI / 180
 }
 
-var mapWidth = 0;
-var mapHeight = 0;
+function movePlayer() {
+	var moveStep = Player.speed * Player.moveSpeed;
+	Player.rotation += Player.direction * Player.rotation;
 
-var miniMapScale = 8;
+	// Calculate player next position
+	var newX = Player.x + Math.cos(Player.rotation) * moveStep;
+	var newY = Player.y + Math.sin(Player.rotation) * moveStep;
+
+	// update position
+	Player.x = newX;
+	Player.y = newY;
+}
 
 function init() {
-
 	mapWidth = map[0].length;
 	mapHeight = map.length;
 
 	bindKeys();
 
-	drawMiniMap();
+	drawMap();
 
 	gameCycle();
 }
 
-// bind keyboard events to game functions (movement, etc)
 function bindKeys() {
-
 	document.onkeydown = function(e) {
 		e = e || window.event;
-
-		switch (e.keyCode) { // which key was pressed?
-
-			case 38: // up, move player forward, ie. increase speed
-				player.speed = 1;
+		
+		switch(e.keyCode) {
+			// Up. Move player forward
+			case 38:
+				Player.speed = 1;
 				break;
-
-			case 40: // down, move player backward, set negative speed
-				player.speed = -1;
+			// Down. Move player backwards
+			case 40:
+				Player.speed = -1;
 				break;
-
-			case 37: // left, rotate player left
-				player.dir = -1;
+			// Left.
+			case 37:
+				Player.direction = -1;
 				break;
-
-			case 39: // right, rotate player right
-				player.dir = 1;
+			// Rigth.
+			case 39:
+				Player.direction = 1;
 				break;
 		}
 	}
-
 	document.onkeyup = function(e) {
 		e = e || window.event;
-
-		switch (e.keyCode) {
+		
+		switch(e.keyCode) {
 			case 38:
 			case 40:
-				player.speed = 0;	// stop the player movement when up/down key is released
+				Player.speed = 0;
 				break;
 			case 37:
 			case 39:
-				player.dir = 0;
+				Player.direction = 0;
 				break;
 		}
 	}
 }
 
-function gameCycle() {
+function drawMap() {
+	// Draw the topdown view minimap
+	var miniMap = $('minimap');
+	// Resize the internal canvas dimensions
+	miniMap.width = mapWidth * miniMapScale;
+	miniMap.height = mapHeight * miniMapScale;
+	// Resize the canvas CSS dimensions
+	miniMap.style.width = (mapWidth * miniMapScale) + 'px';
+	miniMap.style.height = (mapHeight * miniMapScale) + 'px';
 
-	move();
-
-	updateMiniMap();
-
-	setTimeout(gameCycle,1000/30);
-}
-
-function move() {
-	var moveStep = player.speed * player.moveSpeed;	// player will move this far along the current direction vector
-
-	player.rot += player.dir * player.rotSpeed * Math.PI / 180; // add rotation if player is rotating (player.dir != 0)
-
-	var newX = player.x + Math.cos(player.rot) * moveStep;	// calculate new player position with simple trigonometry
-	var newY = player.y + Math.sin(player.rot) * moveStep;
-
-	if (isBlocking(newX, newY)) {	// are we allowed to move to the new position?
-		return; // no, bail out.
+	// Loop through all blocks on the map
+	var ctx = miniMap.getContext('2d');
+	for (var y=0; y < mapHeight; y++) {
+		for (var x=0; x < mapWidth; x++) {
+			var wall = map[y][x];
+			// If there is a wall block at this (x,y)…
+			if (wall > 0) {
+				ctx.fillStyle = 'rgb(150,150,150)';
+				// …Then draw a block on the minimap
+				ctx.fillRect(
+					x * miniMapScale,
+					y * miniMapScale,
+					miniMapScale, miniMapScale
+				);
+			}
+		}
 	}
-
-	player.x = newX; // set new position
-	player.y = newY;
+	drawPlayer();
 }
 
-function isBlocking(x,y) {
-
-	// first make sure that we cannot move outside the boundaries of the level
-	if (y < 0 || y > mapHeight || x < 0 || x > mapWidth)
-		return true;
-
-	// return true if the map block is not 0, ie. if there is a blocking wall.
-	return (map[Math.floor(y)][Math.floor(x)] != 0); 
-}
-
-function updateMiniMap() {
+function drawPlayer() {
 
 	var miniMap = $("minimap");
 	var miniMapObjects = $("minimapobjects");
@@ -142,63 +145,24 @@ function updateMiniMap() {
 	//objectCtx.clearRect(0,0,miniMap.width,miniMap.height);
 
 	objectCtx.fillRect(		// draw a dot at the current player position
-		player.x * miniMapScale - 2, 
-		player.y * miniMapScale - 2,
+		Player.x * miniMapScale - 2, 
+		Player.y * miniMapScale - 2,
 		4, 4
 	);
 	objectCtx.beginPath();
-	objectCtx.moveTo(player.x * miniMapScale, player.y * miniMapScale);
+	objectCtx.moveTo(Player.x * miniMapScale, Player.y * miniMapScale);
 	objectCtx.lineTo(
-		(player.x + Math.cos(player.rot) * 4) * miniMapScale,
-		(player.y + Math.sin(player.rot) * 4) * miniMapScale
+		(Player.x + Math.cos(Player.rotation) * 4) * miniMapScale,
+		(Player.y + Math.sin(Player.rotation) * 4) * miniMapScale
 	);
 	objectCtx.closePath();
 	objectCtx.stroke();
 }
 
-function drawMiniMap() {
-
-	// draw the topdown view minimap
-
-	var miniMap = $("minimap");			// the actual map
-	var miniMapCtr = $("minimapcontainer");		// the container div element
-	var miniMapObjects = $("minimapobjects");	// the canvas used for drawing the objects on the map (player character, etc)
-
-	miniMap.width = mapWidth * miniMapScale;	// resize the internal canvas dimensions 
-	miniMap.height = mapHeight * miniMapScale;	// of both the map canvas and the object canvas
-	miniMapObjects.width = miniMap.width;
-	miniMapObjects.height = miniMap.height;
-
-	var w = (mapWidth * miniMapScale) + "px" 	// minimap CSS dimensions
-	var h = (mapHeight * miniMapScale) + "px"
-	miniMap.style.width = miniMapObjects.style.width = miniMapCtr.style.width = w;
-	miniMap.style.height = miniMapObjects.style.height = miniMapCtr.style.height = h;
-
-	var ctx = miniMap.getContext("2d");
-
-	ctx.fillStyle = "white";
-	ctx.fillRect(0,0,miniMap.width,miniMap.height);
-
-	// loop through all blocks on the map
-	for (var y=0;y<mapHeight;y++) {
-		for (var x=0;x<mapWidth;x++) {
-
-			var wall = map[y][x];
-
-			if (wall > 0) { // if there is a wall block at this (x,y) ...
-
-				ctx.fillStyle = "rgb(200,200,200)";
-				ctx.fillRect(				// ... then draw a block on the minimap
-					x * miniMapScale,
-					y * miniMapScale,
-					miniMapScale,miniMapScale
-				);
-
-			}
-		}
-	}
-
-	updateMiniMap();
+function gameCycle() {
+	movePlayer();
+	drawPlayer();
+	setTimeout(gameCycle,1000/30);	// 30 fps
 }
 
 setTimeout(init, 1);
